64abffbda0e1c6209bfcae1057d5043a2c94eda9
taehwan01 <monte6198@gmail.com>
2024-06-27 14:25:02 +0900

    refactor: 불필요한 파일 제거

--- src/main/java/com/nhnacademy/bookstoreaccount/global/config/WebConfig.java
+++ src/main/java/com/nhnacademy/bookstoreaccount/global/config/WebConfig.java
@@ -1,28 +1,12 @@
 package com.nhnacademy.bookstoreaccount.global.config;
 
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.nhnacademy.bookstoreaccount.global.handler.CustomPageableHandlerMethodArgumentResolver;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-	@Bean
-	public CustomPageableHandlerMethodArgumentResolver customPageableHandlerMethodArgumentResolver() {
-		return new CustomPageableHandlerMethodArgumentResolver();
-	}
-
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(customPageableHandlerMethodArgumentResolver());
-	}
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
